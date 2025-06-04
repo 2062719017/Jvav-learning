079a113c8de3d5e67c96cf500d44907c3239e33a
2062719017 <107345748+2062719017@users.noreply.github.com>
2025-06-04 12:53:26 +0800

    Add files via upload

---
+++ 01-Hello World/src/secondClass.java
@@ -0,0 +1,15 @@
+
+public class secondClass {
+
+	public static void main(String[] args) {
+		System.out.println("secondClass--Hello World");
+	}
+	/**
+	 * @param a	第一个数
+	 * @param b	第二个数
+	 * @return	两数之和
+	 */
+	public static int sum(int a,int b) {
+		return a+b;
+	}
+}
